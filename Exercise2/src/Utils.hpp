3926b3db8bf41c042e8e445ddbc4e997d99bb489
ed3st <s312932@studenti.polito.it>
2025-04-14 19:44:19 +0200

    aggiunta commenti hpp

--- Exercise2/src/Utils.hpp
+++ Exercise2/src/Utils.hpp
@@ -2,22 +2,49 @@
 
 using namespace std;
 
-//ImportData importa corettamente i dati.
+/*
+ImportData importa corettamente i dati.
+	- iFileName è il  path del file di input;
+	- S è il valore iniziale del portafoglio;
+	- n è il numero di asset su cui si investe;
+	- w è il vettore la cui i-esima componente è la frazione di S investita nell'i-esimo asset;
+	- r è il vettore dei rendimenti.
+*/
 bool ImportData( const string& iFileName,
 				 double& S,
 				 size_t& n,
 				 double*& w,
 				 double*& r );
 
-//RateOfReturn calcola il tasso di rendita globale dell'investimento fatto.				 
+
+/*
+RateOfReturn calcola il tasso di rendita globale dell'investimento fatto.
+	- n è il numero di asset su cui si investe;
+	- w è il vettore la cui i-esima componente è la frazione di S investita nell'i-esimo asset;
+	- r è il vettore dei rendimenti.
+*/			 
 double RateOfReturn( const double* const& w,
 					 const double* const& r,
 					 const size_t& n );
 
-//FinalValue calcola il valore finale del portafoglio.
+
+/*FinalValue calcola il valore finale del portafoglio.
+	- S è il valore iniziale del portafoglio;
+	- R è il tasso di rendimento totale.
+*/
 double FinalValue( const double& S, const double& R );
 
-//ExportResult produce correttamente l'output.
+
+/*
+ExportResult produce correttamente l'output.
+	- oFileName è il  path del file di output;
+	- S è il valore iniziale del portafoglio;
+	- n è il numero di asset su cui si investe;
+	- w è il vettore la cui i-esima componente è la frazione di S investita nell'i-esimo asset;
+	- r è il vettore dei rendimenti;
+	- R è il tasso di rendimento totale;
+	- V è il valore finale del portafoglio.
+*/
 bool ExportResult( const string& oFileName,
 				   const double& S,
 				   const size_t& n,
